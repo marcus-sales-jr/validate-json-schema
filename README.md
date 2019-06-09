45e2d8b6f05e8944ad77db251df7366971b46f6e
Marcus Sales <delphi.marcus@gmail.com>
2019-06-08 22:26:49 -0300

    Primeiro Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Validação de Schema Json com PHP
+
+Exemplo de validação de schema json usando PHP.
+
+Para mais informações sobre [json-schema](http://json-schema.org/).
+
+Para mais informações sobre [justinrainbow/json-schema](https://github.com/justinrainbow/json-schema).
+
+
+## Executando localmente
+### Clonando Exemplo
+```bash
+git clone https://github.com/marcus-sales-jr/validate-json-schema.git
+```
+### Instalar dependências com Composer
+[Instalando PHP Composer](https://getcomposer.org/doc/00-intro.md)
+```bash
+composer update
+```
+
+## Executar exemplo 
+```php
+php index.php
+```
---
+++ index.php
@@ -0,0 +1,24 @@
+gi<?php
+require("./vendor/autoload.php");
+
+use JsonSchema\Validator;
+
+$composer = file_get_contents('destaques.json');
+$composerJson  = json_decode($composer);
+
+$composerSchema = file_get_contents('destaques-schema.json');
+$composerSchemaJson  = json_decode($composerSchema);
+
+$validator = new Validator();
+$validator->validate($composerJson,  $composerSchemaJson);
+
+if($validator->isValid()){
+    echo "Schema valido!";
+}else{
+    echo "O documento é inválido. Violações: <br>";
+    echo "<ul>";
+    foreach ($validator->getErrors() as $error) {
+        echo "<li>" . sprintf("[%s] %s\n", $error['property'], $error['message']) . "</li>";
+    }
+    echo "</ul>";
+}
